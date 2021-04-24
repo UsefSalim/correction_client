9ef64b2d0ce460dbf758486447b900f32ea8c3e4
UsefSalim <salimguithub@gmail.com>
2021-04-24 12:46:00 +0000

    client correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import {BrowserRouter as Router ,Route,Switch} from 'react-router-dom'
+import AddContact from './components/AddContact';
+import Reply from './components/Reply';
+import Home from './components/Home'
+const  App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route  path="/add" component={AddContact} />
+        <Route  path="/Reply/:id" component={Reply} />
+      </Switch>
+    </Router>
   );
 }
 
